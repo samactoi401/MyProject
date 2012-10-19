ef63af86e18630cd0f721383e2fbdc7897a5260a
samactoi401 <hy_vong401@yahoo.com>
2012-10-19 19:38:05 +0700

    Update IAM/src/demo/MainApp.java
    123

--- IAM/src/demo/MainApp.java
+++ IAM/src/demo/MainApp.java
@@ -5,7 +5,7 @@ public class MainApp
 	public static void main (String[] args)
 	{
 			System.out.println("This is my project");
-			System.out.println("Hello friends and you1111");
+			System.out.println("Hello friends and you111");
 	}
 
 }
